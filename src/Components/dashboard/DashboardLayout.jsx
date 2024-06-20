3508a306426186ea1abc46c98ef943dcb9ab7e03
muhammadali246397 <muhammadali246397@gmail.com>
2024-06-20 15:16:24 +0600

    making add porject page

--- src/Components/dashboard/DashboardLayout.jsx
+++ src/Components/dashboard/DashboardLayout.jsx
@@ -6,24 +6,36 @@ import { FaFileUpload, FaHome, FaUpload } from "react-icons/fa";
 const DashboardLayout = () => {
     return (
         <div>
-           <div className='grid grid-cols-12 '>
-            <div className=' bg-black text-[#e7e9fb] px-2 h-[100vh] sticky top-0 flex flex-col  col-span-2'>
-                <Link className='w-full px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaCoins /></span> <span className='ms-4 truncate'>Dashboard</span></Link>
-                <Link to='/dashboard/projects' className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaCircleExclamation /></span> <span className='ms-4 truncate'>Projects</span></Link>
-                <Link to='/dashboard/blogs' className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaBlog /></span> <span className='ms-4 truncate'>Blogs</span></Link>
-                <Link to='/dashboard/addProject' className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaUpload /></span> <span className='ms-4 truncate'>Add Project</span></Link>
-                <Link to='/dashboard/addBlogs' className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaFileUpload /></span> <span className='ms-4 truncate'>Add Blogs</span></Link>
-                <Link to="/dashboard/resume"className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaFileImport /></span> <span className='ms-4 truncate'>Upload Resume</span></Link>
-                <Link className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaUser /></span> <span className='ms-4 truncate'>Profile</span></Link>
-                <Link className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaGear /></span> <span className='ms-4 truncate'>Setting</span></Link>
-                <Link to="/" className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaHome /></span> <span className='ms-4 truncate'>Home</span></Link>
-            </div>
-            <div className='col-span-10 bg-[#e7e9fb]'>
-                <div className='mx-10 mt-10'>
-                    <Outlet/>
+            <div className='grid grid-cols-12 '>
+                <div className=' bg-black text-[#e7e9fb] px-2 h-[100vh] sticky top-0 flex flex-col  col-span-2'>
+                    <Link className='w-full px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaCoins /></span> <span className='ms-4 truncate'>Dashboard</span></Link>
+                    <Link to='/dashboard/projects' className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaCircleExclamation /></span> <span className='ms-4 truncate'>Projects</span></Link>
+                    <Link to='/dashboard/blogs' className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaBlog /></span> <span className='ms-4 truncate'>Blogs</span></Link>
+                    <Link to='/dashboard/addProject' className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaUpload /></span> <span className='ms-4 truncate'>Add Project</span></Link>
+                    <Link to='/dashboard/addBlogs' className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaFileUpload /></span> <span className='ms-4 truncate'>Add Blogs</span></Link>
+                    <Link to="/dashboard/resume" className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaFileImport /></span> <span className='ms-4 truncate'>Upload Resume</span></Link>
+                    <Link className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaUser /></span> <span className='ms-4 truncate'>Profile</span></Link>
+                    <Link className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaUser /></span> <span className='ms-4 truncate'>Deleted Projects</span></Link>
+                    <Link className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaUser /></span> <span className='ms-4 truncate'>Deleted Blogs</span></Link>
+                    <Link className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaGear /></span> <span className='ms-4 truncate'>Setting</span></Link>
+                    <Link to="/" className='w-full   px-2 py-3 font-semibold  hover:text-white hover:font-bold text-lg inline-flex items-center'><span><FaHome /></span> <span className='ms-4 truncate'>Home</span></Link>
+                </div>
+                <div className='col-span-10 bg-[#e7e9fb]'>
+                    <div className=''>
+
+                        <div className='flex justify-between bg-white py-4 sticky top-0'>
+                            <div className=''>
+                                <input className='py-2 border rounded-md ms-5 px-2 me-2' type="text" placeholder='Sarch Project' />
+                                <input className='py-2 border rounded-md px-3' type="submit" value='Search' />
+                            </div>
+                            <div>
+                                <FaUser/>
+                            </div>
+                        </div>
+                        <Outlet />
+                    </div>
                 </div>
             </div>
-           </div>
         </div>
     );
 };
--- src/Components/dashboard/addProject/AddProject.jsx
+++ src/Components/dashboard/addProject/AddProject.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AddProject = () => {
     return (
         <div>
-            add projet
+            <h1 className='text-3xl font-bold text-center my-6'>Upload your Project</h1>
         </div>
     );
 };
